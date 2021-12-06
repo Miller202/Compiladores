407b80cd2050d1b46144762c4458202039f5aea1
Michael Miller <mullerg4michael@gmail.com>
2021-12-06 16:45:26 -0300

    Create Token Class

---
+++ Analisadores/src/analisadores/lexico/Token.java
@@ -0,0 +1,22 @@
+package analisadores.lexico;
+
+public class Token {
+
+    public CategTokens category;
+    public String lexeme;
+    public int line, column;
+
+    public Token(CategTokens category, String lexeme, int line, int column) {
+        this.category = category;
+        this.lexeme = lexeme;
+        this.line = line;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        String format = " [%04d,%04d] (%04d,%20s) {%s} ";
+        return String.format(format, line - 1, column, category.ordinal(), category, lexeme);
+    }
+
+}
