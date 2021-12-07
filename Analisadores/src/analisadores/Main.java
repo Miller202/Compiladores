f3ea176a86b6f649b3832f4cf2972742da872347
Michael Miller <mullerg4michael@gmail.com>
2021-12-06 22:05:45 -0300

    Partial NextToken implementation

--- Analisadores/src/analisadores/Main.java
+++ Analisadores/src/analisadores/Main.java
@@ -1,11 +1,25 @@
 package analisadores;
 
 import analisadores.lexico.Lexico;
+import analisadores.lexico.Token;
+
+import java.util.Objects;
 
 public class Main {
 
     public static void main(String[] args) {
 	    Lexico lexico = new Lexico("C:/Users/mulle/OneDrive/Documentos/GitHub/Compiladores/CodigosExemplo/hello_world.txt");
-        System.out.println(lexico.getBufReader());
+        //System.out.println(lexico.getBufReader());
+        Token token;
+        try {
+            do {
+                token = lexico.nextToken();
+                if(token != null) {
+                    System.out.println(token);
+                }
+            }while(!Objects.requireNonNull(token).lexeme.equals("EOF"));
+        }catch(Exception ex) {
+            System.out.println("common error");
+        }
     }
 }
--- Analisadores/src/analisadores/lexico/Lexico.java
+++ Analisadores/src/analisadores/lexico/Lexico.java
@@ -9,6 +9,7 @@ public class Lexico {
     private char[] cont;
     private BufferedReader bufReader;
     private String txtLine = " ";
+    private final WordsHashTable hashTable = new WordsHashTable();
 
     public Lexico(String file){
         try{
@@ -52,4 +53,147 @@ public class Lexico {
     public BufferedReader getBufReader() {
         return bufReader;
     }
+
+    public Token nextToken(){
+        int state = 0;
+        char character;
+        String lexico = "";
+
+        while(true){
+            if(pos == cont.length){
+                if(!readNextLine()){
+                    return new Token(CategTokens.EOF, "EOF", line, column);
+                }else{
+                    cont = txtLine.toCharArray();
+                }
+            }
+
+            character = cont[pos++]; // próximo char
+
+            switch(state){
+                case 0:
+                    if(isWhitespc(character)){
+                        state = 0;
+                    }
+                    else if(Character.isDigit(character)){
+                        lexico += character;
+                        state = 1;
+                    }
+                    else if(Character.isLowerCase(character)){
+                        lexico += character;
+                        state = 5;
+                    }
+                    else if(Character.isUpperCase(character)){
+                        lexico += character;
+                        state = 7;
+                    }
+                    else if(character == ';') {
+                        lexico += character;
+                        column++;
+                        return new Token(CategTokens.TERMINAL, lexico, line, column);
+                    }
+                    else if(character == '(') {
+                        lexico += character;
+                        column++;
+                        return new Token(CategTokens.AB_PAR, lexico, line, column);
+                    }
+                    else if(character == ')') {
+                        lexico += character;
+                        column++;
+                        return new Token(CategTokens.FEC_PAR, lexico, line, column);
+                    }
+                    else {
+                        return new Token(CategTokens.ERR_SYM, lexico, line, column);
+                    }
+                    break;
+                case 1:
+                    if(character == '.') {
+                        lexico += character;
+                        state = 2;
+                        column++;
+                    }
+                    else if(Character.isDigit(character)) {
+                        lexico += character;
+                    }
+                    else if(!Character.isLetterOrDigit(character) || isWhitespc(character)
+                            || isOperation(character)) {
+                        pos--;
+                        state = 3;
+                    }
+                    else {
+                        column++;
+                        new Token(CategTokens.ERR_NUM, lexico, line, column);
+                    }
+                    break;
+                case 2:
+                    if(Character.isDigit(character)) {
+                        lexico += character;
+                    }
+                    else if(!Character.isLetterOrDigit(character) || isWhitespc(character)
+                            || isOperation(character)) {
+                        pos--;
+                        state = 4;
+                    }
+                    else {
+                        column++;
+                        new Token(CategTokens.ERR_NUM, lexico, line, column);
+                    }
+                    break;
+                case 3:
+                    pos--;
+                    column++;
+                    return new Token(CategTokens.CT_INT, lexico, line, column);
+                case 4:
+                    pos--;
+                    column++;
+                    return new Token(CategTokens.CT_FLOAT, lexico, line, column);
+                case 5:
+                    if(Character.isDigit(character) || Character.isLowerCase(character)
+                            || Character.isUpperCase(character)) {
+                        lexico += character;
+                    }
+                    else if(!Character.isLetterOrDigit(character) || isWhitespc(character)
+                            || isOperation(character)) {
+                        pos--;
+                        state = 6;
+                    }
+                    else {
+                        column++;
+                        new Token(CategTokens.ERR_ID, lexico, line, column);
+                    }
+                    break;
+                case 6:
+                    pos--;
+                    column++;
+                    return new Token(CategTokens.ID, lexico, line, column);
+                case 7:
+                    if(Character.isLowerCase(character)) {
+                        lexico += character;
+                    } else if(!Character.isLowerCase(character) || isWhitespc(character)) {
+                        pos--;
+                        state = 8;
+                    }
+                    break;
+                case 8:
+                    pos--;
+                    if(hashTable.reservedWord.get(lexico) == null) {
+                        column++;
+                        return new Token(CategTokens.ERR_PR, lexico, line, column);
+                    }else {
+                        column++;
+                        return new Token(hashTable.reservedWord.get(lexico), lexico, line, column);
+                    }
+            }
+        }
+    }
+
+    private boolean isWhitespc(char character) {
+        return Character.isWhitespace(character) || character == '\n' || character == '\f'
+                || character == '\b' || character == '\0' || character == '\r' || character == '\t';
+    }
+
+    private boolean isOperation(char character) {
+        return  character == '=' || character == '!' || character == '>' || character == '<';
+    }
+
 }
