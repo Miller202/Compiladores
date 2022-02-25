f39beedaa8635de9f3e3382d055524b1cc129ef6
Márcio Henrique <marcioh.ikke@gmail.com>
2022-02-25 19:53:08 -0300

    Update Sintático

--- Analisadores/src/analisadores/sintatico/Sintatico.java
+++ Analisadores/src/analisadores/sintatico/Sintatico.java
@@ -7,24 +7,24 @@ import analisadores.lexico.Token;
 public class Sintatico {
     private Lexico lexico;
     private Token token;
-    private String epsilon = "Epsilon";
+    private String epsilon = "ε";
 
     public Sintatico(String a) {
         lexico = new Lexico(a);
         setNextToken();
+        fS();
     }
 
-    public void setNextToken() {
+    private void setNextToken() {
         lexico.columnUpdate();
         token = lexico.nextToken();
         if(token.category == CategTokens.EOF) {
             System.out.println(token);
             return;
         }
-        FirstProduction();
     }
 
-    public boolean checkCategory(CategTokens... categories) {
+    private boolean checkCategory(CategTokens... categories) {
         for (CategTokens category: categories) {
             if (token.category == category) {
                 return true;
@@ -33,27 +33,82 @@ public class Sintatico {
         return false;
     }
 
-    public void printProduction(String left, String right) {
+    private boolean isTypeCategory() {
+        return checkCategory(CategTokens.PR_INT, CategTokens.PR_FLOAT, CategTokens.PR_CHAR, CategTokens.PR_STR, CategTokens.PR_BOOL);
+    }
+
+    private boolean isConstCategory() {
+        return checkCategory(CategTokens.CT_INT, CategTokens.CT_FLOAT, CategTokens.CT_CHAR, CategTokens.CT_STR);
+    }
+
+    private void printProduction(String left, String right) {
         String format = "%10s%s = %s";
         System.out.printf((format) + "%n", "", left, right);
     }
 
-    public void FirstProduction(){
-        if(checkCategory(CategTokens.PR_MAIN)){
-            printProduction("S", "DcMain");
-            System.out.println(token);
-            setNextToken();
-        }
-        else if(checkCategory(CategTokens.PR_FUNCTION)){
+    private void fS(){
+//        if(checkCategory(CategTokens.PR_MAIN)){
+//            printProduction("S", "DcMain");
+//            System.out.println(token);
+//            setNextToken();
+//            fDcMain();
+//        }
+        if(checkCategory(CategTokens.PR_FUNCTION)){
             printProduction("S", "DcFun S");
             System.out.println(token);
-            setNextToken();
+
+            fDcFun();
+            fS();
         }
-        else{
-            printProduction("S", epsilon);
+        else if(isTypeCategory()){
+            printProduction("S", "DcId S");
             System.out.println(token);
             setNextToken();
+            fDcId();
+            fS();
+        }
+        else {
+            printProduction("S", epsilon);
+            System.out.println(token);
         }
     }
+
+    private void fDcMain () {
+    }
+
+    private void fDcFun () {
+        if(checkCategory(CategTokens.PR_FUNCTION)) {
+            printProduction("DcFun", " ‘Funct’ FunType ‘id’ ‘(‘ Param ‘)’ BlockDc");
+
+            setNextToken();
+            fFunType();
+
+            if (checkCategory(CategTokens.ID)) {
+                setNextToken();
+            }
+            if (checkCategory(CategTokens.AB_PAR)) {
+                setNextToken();
+            }
+
+            fParam();
+
+            if (checkCategory(CategTokens.FEC_PAR)) {
+                setNextToken();
+            }
+
+            if (checkCategory(CategTokens.PR_BEGIN)) {
+                fBlockDc();
+            }
+
+        }
+    }
+
+    private void fFunType() {
+
+    }
+
+
+
+
 
 }
