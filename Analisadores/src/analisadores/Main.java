976120b68f9f168a3bc87335dd1c0e8b985fa75b
Márcio Henrique <marcioh.ikke@gmail.com>
2022-02-28 21:39:42 -0300

    Remoção de Comentários e imports desnecessários

--- Analisadores/src/analisadores/Main.java
+++ Analisadores/src/analisadores/Main.java
@@ -1,23 +1,11 @@
 package analisadores;
 
-import analisadores.lexico.CategTokens;
-import analisadores.lexico.Lexico;
-import analisadores.lexico.Token;
 import analisadores.sintatico.Sintatico;
 
 public class Main {
 
     public static void main(String[] args) {
-	    //Lexico lexico = new Lexico(args[0]);
-        //System.out.println(lexico.getBufReader());
         try {
-//            do {
-//                lexico.columnUpdate();
-//                token = lexico.nextToken();
-//                if(token != null) {
-//                    System.out.println(token);
-//                }
-//            }while(!Objects.requireNonNull(token).category.equals(CategTokens.EOF));
             Sintatico sintatico = new Sintatico(args[0]);
         }catch(Exception ex) {
             System.out.println("common error");
